097c50cbfdba2209d97418e8bd5ea829ca371f38
Your Name <you@example.com>
2023-06-26 19:36:37 +0530

    Code to create pygame window, creating and drawing patient zero, updating patient zero

--- person.py
+++ person.py
@@ -1,3 +1,4 @@
+import pygame
 import random
 
 minMovement = 0.5
@@ -5,6 +6,7 @@ maxSpeed = 5
 
 
 class Person:
+    colors = {"healthy": "white", "sick": "red", "recovered": "blue"}
 
     # status - "healthy" , "sick" or "recovered"
     def __init__(self, x, y, status, socialDistancing):
@@ -16,11 +18,24 @@ class Person:
         self.vx = 0
         self.vy = 0
         self.turnsSick = 0
+        self.recoveryTime = random.randint(100, 150)
 
         if not self.socialDistancing:
             while -minMovement < self.vx < minMovement and -minMovement < self.vy < minMovement:
                 self.vx = random.uniform(-maxSpeed, maxSpeed)
                 self.vy = random.uniform(-maxSpeed, maxSpeed)
+
+    # screen - the main surface
+    def draw(self, screen):
+        pygame.draw.circle(screen, pygame.Color(self.colors[self.status]), (round(self.x), round(self.y)), self.radius)
+
+    # execute once per frame
+    def update(self):
+        self.move()
+        if self.status == "sick":
+            self.turnsSick += 1
+            if self.turnsSick == self.recoveryTime:
+                self.status = "recovered"
 
     def move(self, ):
         if not self.socialDistancing:
---
+++ virusMain.py
@@ -0,0 +1,44 @@
+import pygame
+import random
+from person import *
+
+
+def main():
+    pygame.init()
+    # screen setup
+    WIDTH = HEIGHT = 600
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Virus simulation")
+    screen.fill(pygame.Color('gray'))
+
+    # clock setup
+    clock = pygame.time.Clock()
+    MAX_FPS = 20
+
+    # variables
+    running = True
+    spawnBuffer = 10
+
+    # create people
+    patientZero = Person(random.randint(spawnBuffer, WIDTH - spawnBuffer), random.randint(spawnBuffer, HEIGHT - spawnBuffer), "sick", False)
+
+    while running:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:  # clicked X in top right
+                running = False
+
+        # update people
+        patientZero.update()
+
+        # update graphics
+        screen.fill(pygame.Color('gray'))
+        patientZero.draw(screen)
+        pygame.display.flip()
+
+        # pause for frames
+        clock.tick(MAX_FPS)
+
+    pygame.quit()
+
+
+main()
